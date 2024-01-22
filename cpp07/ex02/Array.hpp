3647678da9f47ae69c9f0a271473aab19f8af8af
elmustapha3 <mustejjamaay2001@gmail.com>
2024-01-22 12:14:37 +0100

    add ex01-cpp08

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -5,47 +5,64 @@
 #define ARRAY_HPP
 
 
-#Include <iostream>
+#include <iostream>
 
 template <class T>
 class Array {
+
 	T * element;
+	unsigned int n;
+
 	public:
 		Array(){
-
+			this->n = 0;
+			this->element = new T[0];
 		};
 
 		Array(unsigned int n){
+			this->n = n;
 			this->element = new T[n];
+			T *t = new T();
+			for (size_t i = 0; i < n; i++)
+			{
+				this->element[i] = *t;
+			}
+			delete t;
+
 		};
 
 		Array(const Array& a){
-			this->element = new T[n];
-			this->element = a.element;
+			this->element = new T[a.n];
+			this->n = a.n;
+			*(this->element) = *(a.element);
 
 		};
 
 		Array& operator=(const Array& a){
-			this->element = a.element;
+			*(this->element) = *(a.element);
 			return *this;
 		};
 
-		int size() const{
-			
+		~Array(){
+			delete [] this->element ;
 		}
 
-		int& operator[](T i){
-			if(i > this->size())
-				throw std::range_error();
-			retrun this->element[i];
+		int size() const{
+			return this->n;
+		}
+
+		T& operator[](int i){
+			if(i >= this->size() || i < 0)
+				throw std::runtime_error("index out of range");
+			return this->element[i];
+		}
+
+		const T& operator[](int i)const{
+			if(i >= this->size() || i < 0)
+				throw std::runtime_error("index out of range");
+			return this->element[i];
 		}
 };
 
-
-Array<int> a(5);
-
-a[0] = 5;
-a[1] =2;
-new T[n]
 
 #endif
---
+++ cpp08/ex01/Span.hpp
@@ -0,0 +1,16 @@
+#ifndef SPAN_HPP
+#define SPAN_HPP
+
+#include <iostream>
+
+class Span {
+	public:
+		Span();
+		Span(unsighed int N);
+		Span(const Span& s);
+		Span& operator=(const Span& s);
+		
+}
+
+
+#endif
