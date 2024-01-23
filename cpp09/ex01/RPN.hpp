43cbbe5d0208b7e5494e9874940c2b6aed646ad6
elmustapha3 <mustejjamaay2001@gmail.com>
2024-01-23 22:55:43 +0100

    add cpp09

---
+++ cpp09/ex01/RPN.hpp
@@ -0,0 +1,18 @@
+#ifndef RPN_HPP
+#define RPN
+
+#include <iostream>
+
+class RPN{
+	std::stack<int> s;
+	public:
+		RPN();
+		RPN(int ac, char **av);
+		RPN(const RPN& rpn);
+		RPN& operator=(const RPN& rpn);
+		void parse(int ac, char **av);
+		void calcule(int ac, char **av);
+		~RPN();
+};
+
+#endif
